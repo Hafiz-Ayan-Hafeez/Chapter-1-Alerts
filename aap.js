f1daee6598f973d3b3048e40d5b979843f2fb764
Hafiz-Ayan-Hafeez <hafiz.ayan.public@gmail.com>
2024-09-15 21:59:22 +0500

    Chapter-1 Done

--- aap.js
+++ aap.js
@@ -6,3 +6,4 @@ alert("Welcome to Js Land");
 alert("Happy Coding!\nPrevent this page from creating additional dialogs.");
 
 console.log("Hello... I can run JS through my web browser's console");
+alert("Hello... I can run JS through my web browser's console");
